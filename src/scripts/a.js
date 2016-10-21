3baf2974c9317d690a7e2899461fe1c44a37525c
linfuxiang <linfuxiang18@163.com>
2016-10-21 11:58:28 +0800

    新文件

---
+++ src/scripts/a.js
@@ -0,0 +1,17 @@
+// 所有模块都通过 define 来定义
+define(function(require, exports, module) {
+  var a = [
+  	{
+  		name: 'ajax',
+  		indexhref: 'htmls/ajax.html',
+  		href: 'ajax.html'
+  	},
+  	{
+  		name: 'vue.js',
+  		indexhref: '',
+  		href: ''
+  	}
+  ];
+  exports.links = a;
+
+});
---
+++ src/scripts/sea-main-config.js
@@ -0,0 +1,14 @@
+seajs.config({
+    // base: "http://localhost/linfuxiang.github.io/dist",
+    base: "./dist",
+    // base: "http://linfuxiang.github.io/dist",
+    alias: {
+      "jquery": "jquery/dist/jquery.min.js",
+      "vue": "vue/dist/vue.min.js",
+      "markdown": "scripts/Markdown.Converter.js"
+    },
+    paths: {
+       // "index-dist": "dist/",
+       // "dist": "http://localhost/linfuxiang.github.io/dist/scripts"
+    }
+});
