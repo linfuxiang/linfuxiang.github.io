ca99d9e44a6de48afca199e444fd2be990f1b085
linfuxiang <linfuxiang18@163.com>
2016-10-22 15:44:19 +0800

    paths

---
+++ src/scripts/paths.js
@@ -0,0 +1,31 @@
+// 所有模块都通过 define 来定义
+define(function(require, exports, module) {
+  var js = '/dist/scripts/';
+  var css = '/dist/css/';
+  var a = [
+    {
+      name: '首页',
+      indexhref: './',
+      href: '/'
+    },
+  	{
+  		name: 'ajax',
+  		// indexhref: 'htmls/ajax.html',
+  		href: '/htmls/ajax.html',
+      details: []
+  	},
+  	{
+  		name: 'Vue.js',
+  		// indexhref: 'htmls/vue.html',
+  		href: '/htmls/vue.html',
+      details: [
+        {
+          name: '数据绑定',
+          href: '/htmls/vueDetails/dataBinding.html'
+        }
+      ]
+  	}
+  ];
+  exports.links = a;
+
+});
