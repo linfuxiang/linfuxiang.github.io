7ab9902ecb54e7ee4be86d141b2aa37732bd2ee8
linfuxiang <linfuxiang18@163.com>
2018-11-14 18:06:08 +0800

    一些文档

--- article/javascript/summary.md
+++ article/javascript/summary.md
@@ -12,7 +12,7 @@
 2. 响应头（Content-Type,Cache-Control,Server...）  
 3. 响应数据  
   
-### HTTPS
+## HTTPS
 在建立TCP/IP链接后对数据进行SSL/TSL加密  
 1. 服务端从证书颁发机构获取证书（公钥、私钥）  
 2. 把公钥发给客户端  
@@ -20,35 +20,63 @@
 4. 服务端使用私钥解密该密码字符串（到此过程为非对称加密）  
 5. 之后的内容全部使用该密码字符串进行加密解密（此过程为对称加密）  
 
-### 生成网页的过程：
+## 生成网页的过程：
 1. DNS解析，找出IP地址（浏览器->本机->域名服务器）  
 2. TCP连接  
 3. HTTP请求（HTTPS则需先进行SSL/TSL加密）  
 4. 浏览器解析HTML构建DOM树，解析CSS构建CSS的渲染树，将两者合并成一个渲染树，然后把它渲染到浏览器窗口上（如果有CSS或JS资源插在其中则会被阻塞）  
-5. JS加载和执行（defer延迟执行和async异步加载）
-	默认： 解析到script标签中断HTML的渲染，去加载和执行JS 
-	defer：异步加载JS，延迟到HTML解析完成之后执行，会阻塞DOMContentLoaded  
-	async：异步加载JS，加载好马上中断HTML的渲染，并执行  
+5. JS加载和执行（defer延迟执行和async异步加载）  
+	`默认`： 解析到script标签中断HTML的渲染，去加载和执行JS  
+	`defer`：异步加载JS，延迟到HTML解析完成之后执行，会阻塞DOMContentLoaded  
+	`async`：异步加载JS，加载好马上中断HTML的渲染，并执行  
+
+## Preload、Prefetch、Preconnect  
+#### Preload  
+对于当前页面很有必要的资源使用 `preload`  
+- 使用了`preload`的资源被赋予高优先级，优先于其他资源加载  
+- 使用`as`属性标明类型  
+
+#### Prefetch    
+对于可能在将来的页面中使用的资源使用 `prefetch`   
+
+- `Link Prefetching` 在浏览器闲置时获取资源并缓存下来  
+
+		<link rel="prefetch" href="//cdn.com/a.js">
+
+- `DNS Prefetching` 提前对某个域名进行DNS解析  
+
+		<link rel="dns-prefetch" href="//example.com">
+
+
+#### Preconnect  
+和`DNS Prefetch`相比，`Preconnect`除了提前进行DNS解析，还会进行TCP握手和建立传输层协议。
+
+		<link rel="preconnect" href="http://example.com">
+
+
+[相关文章1](https://juejin.im/post/58e8acf10ce46300585a7a42)
+[相关文章2](http://bubkoo.com/2015/11/19/prefetching-preloading-prebrowsing/)
 	
-### DOMContentLoaded, load, readystatechange
-**DOMContentLoaded**：在页面被解析完成和静态JS执行完成（JS执行依赖CSS）触发，如果没有JS则在外部CSS加载之前触发  
-**load**：在所有静态资源被加载并执行完成包括（CSS、图片、JS等）  
-**readystatechange**： **document.readyState**有三种状态↓  
+## DOMContentLoaded, load, readystatechange
+`DOMContentLoaded`：在页面被解析完成和静态JS执行完成（JS执行依赖CSS）触发，如果没有JS则在外部CSS加载之前触发  
+`load`：在所有静态资源被加载并执行完成包括（CSS、图片、JS等）  
+`readystatechange`： `document.readyState`有三种状态↓  
 
 		loading：加载中  
 		interactive：页面解析完成，时间上和 DOMContentLoaded 同时发生，不过顺序在它之前  
 		complete：页面上的资源都已加载完毕，时间上和 window.onload 同时发生，不过顺序在他之前  
 
-### reflow和repaint：
+## reflow和repaint：
 回流必定重绘，重绘不一定回流  
-1. reflow（回流）元素的尺寸、结构或某些属性发生改变，浏览器重新渲染部分或全部文档  
-	导致回流：首次渲染，浏览器窗口尺寸发生变化，元素的尺寸内容发生变化...  
-2. repaint（重绘）改变样式并不影响它在文档流中的位置  
 
-### 缓存：  
+- reflow（回流）元素的尺寸、结构或某些属性发生改变，浏览器重新渲染部分或全部文档  
+	导致回流：首次渲染，浏览器窗口尺寸发生变化，元素的尺寸内容发生变化...  
+- repaint（重绘）改变样式并不影响它在文档流中的位置  
+
+## 缓存：  
 设置Cache-Control为max-age=...或no-cache或no-store或must-revalidate  
 过期后验证资源是否有更新（通过Etag）  
 
-Vue 3.0
-1. Proxy
-2. 新的Virtual DOM
+## Vue 3.0  
+1. Proxy  
+2. 新的Virtual DOM  
--- src/scripts/paths.js
+++ src/scripts/paths.js
@@ -4,273 +4,275 @@ define(function(require, exports, module) {
     let css = '/dist/css/';
     let article = '/htmls/article.html';
     let a = [{
-        name: '首页',
-        title: '首页',
-        indexhref: './',
-        href: '/'
-    }, {
-        name: 'vuecli3',
-        title: 'Vue-CLI3',
-        href: article + '?vuecli3',
-        details: [{
-                name: 'theory',
-                title: '分析',
-                href: '?vuecli3-theory'
-            },
-            {
+            name: '首页',
+            title: '首页',
+            indexhref: './',
+            href: '/'
+        }, {
+            name: 'vuecli3',
+            title: 'Vue-CLI3',
+            href: article + '?vuecli3',
+            details: [{
+                    name: 'theory',
+                    title: '分析',
+                    href: '?vuecli3-theory'
+                },
+                {
+                    name: 'plugin',
+                    title: '插件系统',
+                    href: '?vuecli3-plugin'
+                },
+            ]
+        }, {
+            name: 'postcss',
+            title: 'PostCSS',
+            href: article + '?postcss',
+            details: []
+        }, {
+            name: 'parcel',
+            title: 'Parcel',
+            href: article + '?parcel',
+            details: [{
+                name: 'js',
+                title: 'JS编译',
+                href: '?parcel-js'
+            }, {
+                name: 'css',
+                title: 'CSS编译',
+                href: '?parcel-css'
+            }, {
+                name: 'vue',
+                title: 'Vue单文件组件',
+                href: '?parcel-vue'
+            }]
+        }, {
+            name: 'vue2',
+            title: 'Vue2.x',
+            href: article + '?vue2',
+            details: [{
+                name: 'lifecycle',
+                title: '生命周期',
+                href: '?vue2-lifecycle'
+                // }, {
+                //     name: 'vue2-diff',
+                //     title: '2.0与1.0的差异',
+                //     href: '?vue2-diff'
+            }, {
+                name: 'transition',
+                title: 'Transition',
+                href: '?vue2-transition'
+            }, {
+                name: 'directive',
+                title: '指令',
+                href: '?vue2-directive'
+            }, {
                 name: 'plugin',
-                title: '插件系统',
-                href: '?vuecli3-plugin'
-            },
-        ]
-    }, {
-        name: 'postcss',
-        title: 'PostCSS',
-        href: article + '?postcss',
-        details: []
-    }, {
-        name: 'parcel',
-        title: 'Parcel',
-        href: article + '?parcel',
-        details: [{
-            name: 'js',
-            title: 'JS编译',
-            href: '?parcel-js'
+                title: '插件',
+                href: '?vue2-plugin'
+            }, {
+                name: 'ssr',
+                title: 'SSR',
+                href: '?vue2-ssr'
+            }, {
+                name: 'components',
+                title: 'Components',
+                href: '?vue2-components'
+            }, ]
         }, {
-            name: 'css',
-            title: 'CSS编译',
-            href: '?parcel-css'
+            name: 'vuex',
+            title: 'Vuex',
+            href: article + '?vuex',
+            details: [{
+                name: 'state',
+                title: 'State',
+                href: '?vuex-state'
+            }, {
+                name: 'getter',
+                title: 'Getter',
+                href: '?vuex-getter'
+            }, {
+                name: 'mutation',
+                title: 'Mutation',
+                href: '?vuex-mutation'
+            }, {
+                name: 'action',
+                title: 'Action',
+                href: '?vuex-action'
+            }, {
+                name: 'module',
+                title: 'Module',
+                href: '?vuex-module'
+            }, {
+                name: 'plugins',
+                title: '插件',
+                href: '?vuex-plugins'
+            }, {
+                name: 'more',
+                title: '其他',
+                href: '?vuex-more'
+            }, ]
         }, {
-            name: 'vue',
-            title: 'Vue单文件组件',
-            href: '?parcel-vue'
-        }]
-    }, {
-        name: 'vue2',
-        title: 'Vue2.x',
-        href: article + '?vue2',
-        details: [{
-            name: 'lifecycle',
-            title: '生命周期',
-            href: '?vue2-lifecycle'
-            // }, {
-            //     name: 'vue2-diff',
-            //     title: '2.0与1.0的差异',
-            //     href: '?vue2-diff'
+            name: 'vueRouter',
+            title: 'Vue-Router',
+            href: '?vueRouter',
+            details: [{
+                name: 'more',
+                title: '进阶',
+                href: '?vueRouter-more'
+            }, ]
         }, {
-            name: 'transition',
-            title: 'Transition',
-            href: '?vue2-transition'
+            name: 'wxblue',
+            title: '微信硬件蓝牙api（前端开发）',
+            href: article + '?wxblue',
+            details: []
         }, {
-            name: 'directive',
-            title: '指令',
-            href: '?vue2-directive'
+            name: 'css3',
+            title: 'CSS3',
+            href: article + '?css3',
+            details: [{
+                name: 'animation',
+                title: '动画',
+                href: '?css3-animation'
+            }, {
+                name: 'clippath',
+                title: 'Clippath',
+                href: '?css3-clippath'
+            }, {
+                name: 'grid',
+                title: 'Grid',
+                href: '?css3-grid'
+            }]
         }, {
-            name: 'plugin',
-            title: '插件',
-            href: '?vue2-plugin'
+            name: 'javascript',
+            title: 'JavaScript',
+            href: article + '?javascript',
+            details: [{
+                name: 'es',
+                title: 'ES',
+                href: '?javascript-es',
+            }, {
+                name: 'object',
+                title: '对象',
+                href: '?javascript-object',
+            }, {
+                name: 'promise',
+                title: 'Promise',
+                href: '?javascript-promise',
+            }, {
+                name: 'async',
+                title: 'Async Await',
+                href: '?javascript-async',
+            }, {
+                name: 'ajax',
+                title: 'Ajax',
+                href: '?javascript-ajax',
+            }, {
+                name: 'summary',
+                title: 'Summary',
+                href: '?javascript-summary',
+            }]
         }, {
-            name: 'ssr',
-            title: 'SSR',
-            href: '?vue2-ssr'
+            name: 'canvas',
+            title: 'Canvas',
+            href: article + '?canvas',
+            details: [{
+                name: 'shapes',
+                title: '绘制图形',
+                href: '?canvas-shapes',
+            }, {
+                name: 'colors',
+                title: '颜色',
+                href: '?canvas-colors',
+            }, ]
         }, {
-            name: 'components',
-            title: 'Components',
-            href: '?vue2-components'
-        }, ]
-    }, {
-        name: 'vuex',
-        title: 'Vuex',
-        href: article + '?vuex',
-        details: [{
-            name: 'state',
-            title: 'State',
-            href: '?vuex-state'
+            name: 'typescript',
+            title: 'TypeScript',
+            href: article + '?typescript',
+            details: [{
+                name: 'typeof',
+                title: '基础类型',
+                href: '?typescript-typeof',
+            }, {
+                name: 'interface',
+                title: '接口',
+                href: '?typescript-interface',
+            }, {
+                name: 'class',
+                title: '类',
+                href: '?typescript-class',
+            }, {
+                name: 'function',
+                title: '函数',
+                href: '?typescript-function',
+            }, {
+                name: 'enum',
+                title: '枚举',
+                href: '?typescript-enum',
+            }, {
+                name: 'modules',
+                title: '模块',
+                href: '?typescript-modules',
+            }, {
+                name: 'namespace',
+                title: '命名空间',
+                href: '?typescript-namespace',
+            }, {
+                name: 'jsx',
+                title: 'JSX',
+                href: '?typescript-jsx',
+            }, {
+                name: 'API',
+                title: '开发文档',
+                href: 'https://www.tslang.cn/docs/handbook/basic-types.html',
+                isBlank: true
+            }, ]
         }, {
-            name: 'getter',
-            title: 'Getter',
-            href: '?vuex-getter'
+            name: 'jest',
+            title: 'Jest',
+            href: article + '?jest',
+            details: []
         }, {
-            name: 'mutation',
-            title: 'Mutation',
-            href: '?vuex-mutation'
+            name: 'sea',
+            title: 'Sea',
+            href: article + '?sea',
+            details: []
         }, {
-            name: 'action',
-            title: 'Action',
-            href: '?vuex-action'
+            name: 'node',
+            title: 'Node.js',
+            href: article + '?node',
+            details: [{
+                name: 'express',
+                title: 'Express',
+                href: '?node-express'
+            }]
         }, {
-            name: 'module',
-            title: 'Module',
-            href: '?vuex-module'
+            name: 'mongoDB',
+            title: 'mongoDB数据库',
+            href: article + '?mongoDB',
+            details: []
         }, {
-            name: 'plugins',
-            title: '插件',
-            href: '?vuex-plugins'
+            name: 'linux',
+            title: 'Linux系统操作',
+            href: article + '?linux',
+            details: []
         }, {
-            name: 'more',
-            title: '其他',
-            href: '?vuex-more'
-        }, ]
-    }, {
-        name: 'vueRouter',
-        title: 'Vue-Router',
-        href: '?vueRouter',
-        details: [{
-            name: 'more',
-            title: '进阶',
-            href: '?vueRouter-more'
-        }, ]
-    }, {
-        name: 'wxblue',
-        title: '微信硬件蓝牙api（前端开发）',
-        href: article + '?wxblue',
-        details: []
-    }, {
-        name: 'css3',
-        title: 'CSS3',
-        href: article + '?css3',
-        details: [{
-            name: 'animation',
-            title: '动画',
-            href: '?css3-animation'
+            name: 'sublime',
+            title: 'Sublime Text 3',
+            href: article + '?sublime',
+            details: []
         }, {
-            name: 'clippath',
-            title: 'Clippath',
-            href: '?css3-clippath'
-        }, {
-            name: 'grid',
-            title: 'Grid',
-            href: '?css3-grid'
-        }]
-    }, {
-        name: 'javascript',
-        title: 'JavaScript',
-        href: article + '?javascript',
-        details: [{
-            name: 'es',
-            title: 'ES',
-            href: '?javascript-es',
-        }, {
-            name: 'object',
-            title: '对象',
-            href: '?javascript-object',
-        }, {
-            name: 'promise',
-            title: 'Promise',
-            href: '?javascript-promise',
-        }, {
-            name: 'async',
-            title: 'Async Await',
-            href: '?javascript-async',
-        }, {
-            name: 'ajax',
-            title: 'Ajax',
-            href: '?javascript-ajax',
-        }, {
-            name: 'summary',
-            title: 'Summary',
-            href: '?javascript-summary',
-        }]
-    }, {
-        name: 'canvas',
-        title: 'Canvas',
-        href: article + '?canvas',
-        details: [{
-            name: 'shapes',
-            title: '绘制图形',
-            href: '?canvas-shapes',
-        }, {
-            name: 'colors',
-            title: '颜色',
-            href: '?canvas-colors',
-        }, ]
-    }, {
-        name: 'typescript',
-        title: 'TypeScript',
-        href: article + '?typescript',
-        details: [{
-            name: 'typeof',
-            title: '基础类型',
-            href: '?typescript-typeof',
-        }, {
-            name: 'interface',
-            title: '接口',
-            href: '?typescript-interface',
-        }, {
-            name: 'class',
-            title: '类',
-            href: '?typescript-class',
-        }, {
-            name: 'function',
-            title: '函数',
-            href: '?typescript-function',
-        }, {
-            name: 'enum',
-            title: '枚举',
-            href: '?typescript-enum',
-        }, {
-            name: 'modules',
-            title: '模块',
-            href: '?typescript-modules',
-        }, {
-            name: 'namespace',
-            title: '命名空间',
-            href: '?typescript-namespace',
-        }, {
-            name: 'jsx',
-            title: 'JSX',
-            href: '?typescript-jsx',
-        }, {
-            name: 'API',
-            title: '开发文档',
-            href: 'https://www.tslang.cn/docs/handbook/basic-types.html',
-            isBlank: true
-        }, ]
-    }, {
-        name: 'jest',
-        title: 'Jest',
-        href: article + '?jest',
-        details: []
-    }, {
-        name: 'sea',
-        title: 'Sea',
-        href: article + '?sea',
-        details: []
-    }, {
-        name: 'node',
-        title: 'Node.js',
-        href: article + '?node',
-        details: [{
-            name: 'express',
-            title: 'Express',
-            href: '?node-express'
-        }]
-    }, {
-        name: 'mongoDB',
-        title: 'mongoDB数据库',
-        href: article + '?mongoDB',
-        details: []
-    }, {
-        name: 'linux',
-        title: 'Linux系统操作',
-        href: article + '?linux',
-        details: []
-    }, {
-        name: 'sublime',
-        title: 'Sublime Text 3',
-        href: article + '?sublime',
-        details: []
-    }, {
-        name: 'little',
-        title: 'Something',
-        href: article + '?little',
-        details: []
-    }, {
-        name: 'net',
-        title: '网络相关',
-        href: article + '?net',
-        details: []
-    }, ];
+            name: 'net',
+            title: '网络相关',
+            href: article + '?net',
+            details: []
+        },
+        // {
+        //     name: 'little',
+        //     title: 'Something',
+        //     href: article + '?little',
+        //     details: []
+        // }, 
+    ];
     // exports.links = a;
     module.exports = a;
 });
