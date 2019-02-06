a5c763c17029ac87440a45e7d9a9eaf2e86368bc
linfuxiang <linfuxiang18@163.com>
2019-02-06 23:17:48 +0800

    update

--- src/scripts/birthday.js
+++ src/scripts/birthday.js
@@ -1,19 +1,31 @@
-let list = [{
+const list = [{
 	date: '2018.11.02.',
 	img: '/dist/img_sp/20181102.jpg',
-	desc: '我们第一次加微信聊天。',
+	desc: '我们加了微信，说了第一句问候语，任静哈。',
+	init: false,
+}, {
+	date: '2018.11.10.',
+	img: '/dist/img_sp/20181110.jpg',
+	desc: '我们第一次看电影。',
+	init: false,
 }, {
 	date: '2018.11.18.',
-	img: '/dist/img_sp/20181118.jpg',
+	img: [
+		'/dist/img_sp/20181118_1.jpg',
+		'/dist/img_sp/20181118_2.jpg',
+	],
 	desc: '你第一次陪我去医院，让我感受到了温暖，同时也保留下了我第一张黑照。',
+	init: false,
 }, {
 	date: '2018.11.25.',
 	img: '/dist/img_sp/20181125.jpg',
 	desc: '我们去了云台花园和白云山，第一次牵了你的手。',
+	init: false,
 }, {
 	date: '2018.12.09.',
 	img: '/dist/img_sp/20181209.jpg',
 	desc: '我们第一次去红专厂拍照。',
+	init: false,
 }, {
 	date: '2018.12.25.',
 	img: [
@@ -22,6 +34,7 @@ let list = [{
 		'/dist/img_sp/20181225_3.jpg',
 	],
 	desc: '圣诞节',
+	init: false,
 }, {
 	date: '2019.01.01.',
 	img: [
@@ -29,18 +42,26 @@ let list = [{
 		'/dist/img_sp/20190101_2.jpg',
 	],
 	desc: '元旦',
+	init: false,
 }, {
 	date: '2019.01.17.',
 	img: '/dist/img_sp/20190117.jpg',
 	desc: '第二天你就要回湖北了，超级不舍，所以给你留了纪念。',
+	init: false,
 }, {
 	date: '2019.01.19.',
 	img: '/dist/img_sp/20190119.jpg',
 	desc: '我们第一次视频聊天，我看到了一个不一样的你。',
+	init: false,
+}, {
+	date: '未来',
+	img: '/dist/img_sp/love.png',
+	desc: '想跟你留下更多回忆，想跟你一直走下去。',
+	init: false,
 }]
 
-var x, y;
-var isStarted = false;
+let x, y;
+let isStarted = false;
 
 // 开始展示图片列表
 function start() {
@@ -52,13 +73,26 @@ function start() {
 	let str = '';
 	for (let i = 0; i < list.length; i++) {
 		let img = '';
-		if(typeof list[i].img === 'object') {
-			for(let j = 0; j < list[i].img.length; j++) {
-				img += `<img src="${ list[i].img[j] }" alt="" class="list-img">`;
+		if (list[i].img && typeof list[i].img === 'object') {
+			if (i < 2) {
+				for (let j = 0; j < list[i].img.length; j++) {
+					img += `<img src="${ list[i].img[j] }" alt="" class="list-img"">`;
+				}
+				list[i].init = true;
+			} else {
+				for (let j = 0; j < list[i].img.length; j++) {
+					img += `<img data-src="${ list[i].img[j] }" alt="" class="list-img" src="/dist/img_sp/love.png">`;
+				}
 			}
-		} else {
-			img = `<img src="${ list[i].img }" alt="" class="list-img">`;
+		} else if (list[i].img) {
+			if (i < 2) {
+				img = `<img src="${ list[i].img }" alt="" class="list-img">`;
+				list[i].init = true;
+			} else {
+				img = `<img data-src="${ list[i].img }" alt="" class="list-img" src="/dist/img_sp/love.png">`;
+			}
 		}
+
 		str += `<li class="li">
 			<div class="list-icon">
 				<div class="list-icon-ver"></div>
@@ -75,8 +109,8 @@ function start() {
 	}
 	$second.innerHTML = str;
 
-
 	$first.className = 'left';
+	// 监听切换动画结束
 	$first.addEventListener("animationend", function(e) {
 		$first.style.display = 'none';
 		// $second.style.display = 'block';
@@ -85,18 +119,56 @@ function start() {
 		isStarted = true;
 	});
 
-
+	// 图片点击放大效果
 	let $imgDefault = document.querySelector('.img-default');
 	$second.addEventListener('click', function(e) {
-		console.log(e.target.src);
+		e.preventDefault();
 		if (e.target.tagName.toLowerCase() === 'img') {
 			$imgDefault.style.backgroundImage = `url('${ e.target.src }')`;
 			$imgDefault.style.display = 'block';
 		}
 	});
+	// $imgDefault.addEventListener('touchmove', function(e) {
+	// 	e.stopPropagation();	
+	// })
+	// 再次点击图片返回
 	$imgDefault.addEventListener('click', function(e) {
 		$imgDefault.style.backgroundImage = '';
 		$imgDefault.style.display = 'none';
+	});
+	// 列表滚动加载图片（函数节流）
+	let flag = false;
+	window.addEventListener('scroll', function() {
+		if (flag) {
+			return false;
+		}
+		flag = true;
+		setTimeout(function() {
+			let scrollY = window.scrollY,
+				clientHeight = document.documentElement.clientHeight;
+			for (let i = 0; i < list.length; i++) {
+				if (!list[i].init) {
+					if (!list[i].el) {
+						list[i].el = document.querySelectorAll('#second li')[i];
+						list[i].img = list[i].el.querySelectorAll('img');
+					}
+					let li = list[i].el;
+					// console.log(li.getBoundingClientRect());
+					let rect = li.getBoundingClientRect(),
+						y = rect.y,
+						height = rect.height;
+					// console.log(i, rect, y, scrollY);
+					console.log(i);
+					if (y + height > 0 && y < clientHeight - 30) {
+						list[i].img.forEach(function(item) {
+							item.src = item.getAttribute('data-src');
+						});
+						list[i].init = true;
+					}
+				}
+			}
+			flag = false;
+		}, 250);
 	});
 }
 
